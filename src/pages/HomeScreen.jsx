e0c84a4642f21f5661b213d731c5afba164dbea9
Ammersken Nils <nilsammersken@gmail.com>
2021-06-10 23:02:24 +0200

    Add typeahead feature unstyled

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,25 +1,37 @@
 import styled from 'styled-components/macro'
 import Header from '../components/Header/Header'
 import Card from '../components/Card/Card'
+import SearchBar from '../components/SearchBar/SearchBar'
 import { ReactComponent as PlusIcon } from '../assets/PlusIcon.svg'
 import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
-import SearchBar from '../components/SearchBar/SearchBar'
 
 export default function HomeScreen({ navigateAdd, cards }) {
   const [searchValue, setSearchValue] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const [typeaheadResults, setTypeaheadResults] = useState([])
+  const [isResultSelected, setIsResultSelected] = useState(false)
 
   useEffect(() => {
     const filteredCards = cards.filter(card =>
       card.fullName.toLowerCase().includes(searchValue.toLowerCase())
     )
     setSearchResults(filteredCards)
+    if (searchValue) setTypeaheadResults(filteredCards)
   }, [cards, searchValue, setSearchValue])
 
-  function onSearchChange(event) {
-    setSearchValue(event.target.value)
+  function onSearchChange(e) {
+    const inputValue = e.target.value
+    setSearchValue(inputValue)
+    setIsResultSelected(false)
+    setTypeaheadResults([])
   }
+
+  function onResultSelected(selectedResult) {
+    setSearchValue(selectedResult)
+    setIsResultSelected(true)
+  }
+
   return (
     <Grid>
       <Header text="pandemeet" />
@@ -35,6 +47,19 @@ export default function HomeScreen({ navigateAdd, cards }) {
               address={card.address}
             />
           ))}
+        <ul>
+          {!isResultSelected &&
+            typeaheadResults.length > 0 &&
+            typeaheadResults.map(result => (
+              <li
+                key={uuidv4()}
+                onClick={() => onResultSelected(result.fullName)}
+                role="button"
+              >
+                {result.fullName}
+              </li>
+            ))}
+        </ul>
       </CardGrid>
       <CreateButton>
         <PlusIcon onClick={navigateAdd} role="button" name="addButton" />
